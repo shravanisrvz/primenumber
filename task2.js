9eb4e45e7b0e63c29fea32f042422c3632a4b362
shravanisrvz <shravanikanuganti13@gmail.com>
2022-03-29 11:01:41 +0530

    https://github.com/shravanisrvz/character-and-count/tree/master/github

---
+++ task2.js
@@ -0,0 +1,8 @@
+var a1 = [10, 20];
+var b1 = [10, 20];
+console.log(a1 == b1); //false
+console.log(a1 === b1); //false
+var c1 = a1;
+//same object  
+console.log(a1 === c1); //true    
+console.log(a1 == c1); //true
---
+++ task2.ts
@@ -0,0 +1,10 @@
+let a1 = [10,20]
+let b1 = [10,20]
+ 
+console.log(a1==b1);     //false
+console.log(a1===b1);    //false
+ 
+let c1=a1
+               //same object  
+console.log(a1===c1);    //true    
+console.log(a1==c1);    //true
